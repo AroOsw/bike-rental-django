8db046673f6a314d99ebf66dbf8d1853e66599e4
AroOsw <128989508+AroOsw@users.noreply.github.com>
2025-07-02 10:43:24 +0700

    Update settings.py to use .env

--- WildWheel/WildWheel/settings.py
+++ WildWheel/WildWheel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h-asixbeu13rx+zb=nhaw)4+1&itz49cj5%glk#lvltg9c%*=$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [

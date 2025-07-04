5c18e6c2ccbd5da01d222ad12dab47ec3c73aa9f
AroOsw <128989508+AroOsw@users.noreply.github.com>
2025-07-04 10:21:38 +0700

    Add templates, views, URLs for core pages

--- WildWheel/core/urls.py
+++ WildWheel/core/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("bikes/", views.bikes, name="bikes"),
+    path("reservations/", views.reservations, name="reservations"),
+    path("contact/", views.contact, name="contact"),
+    path("routes/", views.routes, name="routes"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
 ]
--- WildWheel/core/views.py
+++ WildWheel/core/views.py
@@ -10,3 +10,23 @@ def index(request):
 def bikes(request):
     """Render the bikes page."""
     return render(request, "bikes.html", {})
+
+def routes(request):
+    """Render the routes page."""
+    return render(request, "routes.html", {})
+
+def reservations(request):
+    """Render the reservations page."""
+    return render(request, "reservations.html", {})
+
+def contact(request):
+    """Render the contact page."""
+    return render(request, "contact.html", {})
+
+def login(request):
+    """Render the login page."""
+    return render(request, "login.html", {})
+
+def register(request):
+    """Render the register page."""
+    return render(request, "register.html", {})

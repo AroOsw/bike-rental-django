0185e8fe11b7ddb81b647edb0315bf4416d4db29
AroOsw <128989508+AroOsw@users.noreply.github.com>
2025-10-26 13:38:45 +0700

    fix: Correct DATABASES variable in settings.py again

--- WildWheel/settings.py
+++ WildWheel/settings.py
@@ -109,27 +109,23 @@ WSGI_APPLICATION = 'WildWheel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#         'TEST': {
-#             'NAME': 'test_bike_rental',
-#         },
-#     }
-# }
-
-database_url = os.getenv('DATABASE_URL')
-
 DATABASES = {
-    "default": {
-        dj_database_url.parse(database_url, conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'TEST': {
+            'NAME': 'test_bike_rental',
+        },
     }
 }
+
+database_url = os.getenv('DATABASE_URL')
+DATABASES["default"] = dj_database_url.parse(database_url)
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {

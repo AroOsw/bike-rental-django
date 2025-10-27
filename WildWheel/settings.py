f731fc34e4ad7793b0c6b91b5a5ea358c05a67a2
AroOsw <128989508+AroOsw@users.noreply.github.com>
2025-10-27 13:03:03 +0700

    fix:Add whitenoise in settings.py

--- WildWheel/settings.py
+++ WildWheel/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -230,3 +231,4 @@ ACCOUNT_RATE_LIMITS = {
 
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
